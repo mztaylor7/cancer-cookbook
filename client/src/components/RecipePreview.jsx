05cfd7e7e962081b7272e7d4e48405dd89dcc34f
Michael Taylor <cardzfan1207@gmail.com>
2020-08-12 22:57:59 -0400

    Updated with recipe preview component

---
+++ client/src/components/RecipePreview.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import '../styles.scss';
+import styled from 'styled-components';
+
+const RecipePreview = () => {
+
+
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+
+`
+
+export default RecipePreview;
