ccb9109f29c77405afc55be2bba3ca1e2113df13
Michael Taylor <cardzfan1207@gmail.com>
2020-08-12 23:56:04 -0400

    Added styling to recipe preview blocks

--- client/src/components/RecipePreview.jsx
+++ client/src/components/RecipePreview.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import '../styles.scss';
 import styled from 'styled-components';
 
-const RecipePreview = () => {
-
+const RecipePreview = (props) => {
+  const { recipe } = props;
 
   return (
     <Container>
       <ImageCon>
-
+        <ImageHolder>
+          <img  src={recipe.image}/>
+        </ImageHolder>
       </ImageCon>
       <Description>
 
@@ -21,13 +23,30 @@ const RecipePreview = () => {
 }
 
 const Container = styled.div`
-
+  position: relative;
+  display: inline-block;
+  margin: .5rem;
+  width: calc(25% - 1rem);
 `
 const ImageCon = styled.div`
-
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: auto;
+  border-radius: 4px;
+  margin-bottom: .5rem;
 `
 const Title = styled.div`
 
+`
+
+const ImageHolder = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
 `
 
 export default RecipePreview;
