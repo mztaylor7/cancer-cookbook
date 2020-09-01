383d062e5889d8148a621177d9d4247c35b6a7a7
Michael Taylor <cardzfan1207@gmail.com>
2020-08-31 23:45:31 -0400

    Modal skeleton created

--- client/src/components/RecipeModal.jsx
+++ client/src/components/RecipeModal.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const RecipeMOdal = ({isShowing, hide}) => isShowing ? ReactDOM.createPortal(
+const RecipeModal = ({isShowing, hide}) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
     <div className="modal-overlay"/>
     <div className="modal-wrapper">
-
+      <div className="modal">
+        <div className="modal-header">
+          <button type="button" className="modal-close-button" onClick={hide}>
+            <span>X</span>
+          </button>
+        </div>
+      </div>
     </div>
   </React.Fragment>
 )
+
+export default RecipeModal;
